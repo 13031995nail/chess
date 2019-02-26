52f06cc1c4b0cd66021ca9c4ddc6268bea734113
13031995nail <valiullin-nail@list.ru>
2019-02-26 18:06:48 +0500

    'dsd'

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 
 from django.contrib import admin
 
-admin.autodiscover()
-
 import hello.views
 
 # To add a new path, first import the app:
@@ -16,6 +14,5 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
