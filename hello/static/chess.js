062d4368e3f6141bdf5deacae8dcafec3b4b684d
13031995nail <valiullin-nail@list.ru>
2019-02-27 16:20:33 +0500

    'fff'

--- hello/static/chess.js
+++ hello/static/chess.js
@@ -105,7 +105,6 @@ function isBlackSquare(coord){
 }
 
 function progress(position1, position2){
-	start = arr[position1%8]+(8-(Math.floor(position1/8)));
 	end = arr[position2%8]+(8-(Math.floor(position2/8)));
-	document.location.href = "https://hidden-harbor-40615.herokuapp.com/" + start + end;
+	document.location.href = "https://hidden-harbor-40615.herokuapp.com/" + end;
 }
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import chess
 from .models import Greeting
 
+board = chess.Board()
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
-def index1(request, hood):
-    # return HttpResponse('Hello from Python!')
-    return HttpResponse('Hello %s' % hood)
+def index1(request, move):
+    board.push_san(move)
+    return HttpResponse(board.fen())
