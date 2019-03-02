0e35f764828d480b299f6fef0275e0c89bd250a9
13031995nail <valiullin-nail@list.ru>
2019-03-02 18:50:26 +0500

    'ff'

--- hello/static/chess.js
+++ hello/static/chess.js
@@ -110,7 +110,7 @@ function progress(position1, position2){
 	end = arr[position2%8]+(8-(Math.floor(position2/8)));
     $.ajax({
         type: "POST",
-  	    url: '/test',
+  	    url: 'test',
   	    data:{nail:end}
     });
 	//document.location.href = "https://hidden-harbor-40615.herokuapp.com/" + end;
