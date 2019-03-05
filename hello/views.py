decb3a77362475f1823358f41cab5d3c4f3855d3
13031995nail <valiullin-nail@list.ru>
2019-03-05 15:40:24 +0500

    'gg'

--- hello/views.py
+++ hello/views.py
@@ -34,7 +34,7 @@ def index1(request, move):
             stroke += str(1)
         mystr = re.sub(str(i), stroke, mystr)
     moves.append(move)
-#   else:
+##   else:
 #       mystr = computerMove(board, depth).fen()
 #       mystr = mystr[:mystr.find(" ")]
 #       mystr = re.sub(r"[/]", "", mystr)
